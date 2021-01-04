c8470fcb2711380cceadac203ef12736a6eb1a8a
ruobaole <daisyrpanglive@live.com>
2021-01-04 19:02:03 +0800

    add: pixi stage, viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
+import { Stage, Sprite } from '@inlet/react-pixi';
+import { Viewport } from './Viewport';
+import exampleImage from './example_data/simple002.jpeg';
 
 function App() {
+  const [imagePath, setImagePath] = useState<string>(exampleImage);
+  const [stageWidth, setStageWidth] = useState<number>(256);
+  const [stageHeight, setStageHeight] = useState<number>(256);
+  const stageRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (stageRef && stageRef.current) {
+      setStageWidth(stageRef.current.offsetWidth);
+      setStageHeight(stageRef.current.offsetHeight);
+      // console.log(
+      //   `${stageRef.current.offsetWidth} * ${stageRef.current.offsetHeight}`
+      // );
+    }
+  }, [stageRef]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <main className="App-main">
+        <div className="Stage" ref={stageRef}>
+          <Stage width={stageWidth} height={stageHeight}>
+            <Viewport width={200} height={100}>
+              <Sprite image={imagePath} x={0} y={0} />
+            </Viewport>
+          </Stage>
+        </div>
+      </main>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
---
+++ src/Viewport.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as PIXI from 'pixi.js';
+import { PixiComponent, useApp } from '@inlet/react-pixi';
+import { Viewport as PixiViewport } from 'pixi-viewport';
+
+export interface ViewportProps {
+  width: number;
+  height: number;
+  children?: React.ReactNode;
+}
+
+export interface PixiComponentViewportProps extends ViewportProps {
+  app: PIXI.Application;
+}
+
+const PixiComponentViewport = PixiComponent('Viewport', {
+  create: (props: PixiComponentViewportProps) => {
+    const viewport = new PixiViewport({
+      screenWidth: props.width,
+      screenHeight: props.height,
+      worldWidth: props.width * 2,
+      worldHeight: props.height * 2,
+      ticker: props.app.ticker,
+      interaction: props.app.renderer.plugins.interaction,
+    });
+    viewport.drag().pinch().wheel().clampZoom();
+
+    return viewport;
+  },
+});
+
+export const Viewport = (props: ViewportProps) => {
+  const app = useApp();
+  return <PixiComponentViewport app={app} {...props} />;
+};
+
+// export default Viewport;
