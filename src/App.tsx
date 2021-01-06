9a736e8c2dd878a14f4472ce648d9ea252d6b7c4
daisy.pang <daisyrpanglive@live.com>
2021-01-06 20:24:51 +0800

    add: KP push and pop control

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { InsertKPGTool } from './InsertKPGTool';
 import { ClickEventData } from 'pixi-viewport';
 import exampleImage from './example_data/simple002.jpeg';
 import labelingConfig from './labeling_config.json';
-import { State } from 'pixi.js';
 
 // store states
 type SetupState = {
@@ -47,7 +46,7 @@ type LabelState = {
   set: (fn: (state: LabelState) => void) => void;
 };
 const useLabelStore = create<LabelState>((set) => ({
-  keypointGraphList: [],
+  keypointGraphList: [[]],
   curKPG: 0,
   curKP: 0,
   set: (fn) => set(produce(fn)),
@@ -59,11 +58,14 @@ const labelSelector = (state: LabelState) => ({
   setLabelState: state.set,
 });
 
+const KPGMold = labelingConfig.keypointGraph;
 const kpLen = labelingConfig.keypointGraph.length;
 
 function App() {
   const { imagePath, stageSize, setStageSize } = useSetupStore(setupSelector);
-  const { curKPG, curKP, setLabelState } = useLabelStore(labelSelector);
+  const { curKPG, curKP, keypointGraphList, setLabelState } = useLabelStore(
+    labelSelector
+  );
   const [panMode, setPanMode] = useState<boolean>(false);
   const [toolMode, setToolMode] = useState<string>('i');
   const [kx, setKx] = useState<number>(80);
@@ -100,19 +102,52 @@ function App() {
         console.log(`[EVENT]leftmouse ${e.world.x}, ${e.world.y}`);
         setKx(e.world.x);
         setKy(e.world.y);
+        // add keypoint
+        const newKP = {
+          name: KPGMold[curKP].name,
+          x: e.world.x,
+          y: e.world.y,
+          properties: {
+            is_visible: {
+              type: 'boolean',
+              value: true,
+            },
+          },
+        };
+        if (
+          curKP === kpLen - 1 &&
+          keypointGraphList[curKPG].length === curKP + 1
+        ) {
+          // tmp: do nothing when current KPG is already full --> push/pop KPG should be controled with button
+          console.log(`curKPG is already FULL!`);
+        } else {
+          setLabelState((state) => {
+            state.keypointGraphList[curKPG].push(newKP);
+          });
+        }
         setLabelState((state) => {
           state.curKP = curKP + 1 < kpLen ? curKP + 1 : kpLen - 1;
         });
       } else if (e.event.data.button === 2) {
         // right click
+        console.log(`[EVENT]rightmouse ${e.world.x}, ${e.world.y}`);
+        // pop keypoint
+        if (curKP === 0) {
+          // tmp: do nothing when current KPG is already empty
+          console.log(`curKPG is already EMPTY!`);
+        } else {
+          setLabelState((state) => {
+            state.keypointGraphList[curKPG].pop();
+          });
+        }
         setLabelState((state) => {
           state.curKP = curKP - 1 >= 0 ? curKP - 1 : 0;
         });
-        console.log(`[EVENT]rightmouse ${e.world.x}, ${e.world.y}`);
       }
     }
   }
-  console.log(`PAN: ${panMode}`);
+  // console.log(`PAN: ${panMode}`);
+  console.log(keypointGraphList);
   return (
     <div className="App">
       <header className="App-header"></header>
