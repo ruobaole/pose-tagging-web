7e1d7e8993bcb4f5508dfc0bddbe42dc7191b12c
daisy.pang <daisyrpanglive@live.com>
2021-01-07 18:12:27 +0800

    add: keypoint onmouseover and out event

--- src/Keypoint.tsx
+++ src/Keypoint.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
+import * as PIXI from 'pixi.js';
 import { Graphics } from '@inlet/react-pixi';
 
 interface IKeypointProps {
@@ -10,11 +11,12 @@ interface IKeypointProps {
 }
 
 export function Keypoint(props: IKeypointProps) {
-  const radius = props.radius && 4;
-  const color = props.color === undefined ? 0xff00ff : props.color;
-  const alpha = props.alpha === undefined ? 1 : props.alpha;
+  const [showText, setShowText] = useState(false);
   const draw = useCallback(
-    (g) => {
+    (g: PIXI.Graphics) => {
+      const radius = props.radius === undefined ? 4 : props.radius;
+      const color = props.color === undefined ? 0xff00ff : props.color;
+      const alpha = props.alpha === undefined ? 1 : props.alpha;
       g.clear();
       // outer circle
       g.lineStyle(1, color, alpha);
@@ -23,9 +25,41 @@ export function Keypoint(props: IKeypointProps) {
       // center
       g.beginFill(0xffff0b, 0.7);
       g.drawCircle(props.x, props.y, 1);
+      // text -- tmp
+      if (showText) {
+        const label = new PIXI.Text(`2-left_eye-visible`, {
+          fontFamily: 'Arial',
+          fontSize: 14,
+          fontWeight: 'bold',
+          fill: '#ffb700',
+          dropShadow: true,
+          dropShadowColor: '#888888',
+          dropShadowBlur: 4,
+          dropShadowAngle: Math.PI / 6,
+          dropShadowDistance: 6,
+        });
+        label.x = props.x - 4;
+        label.y = props.y - 2;
+        g.addChild(label);
+      } else {
+        g.removeChildren();
+      }
       g.endFill();
     },
-    [props]
+    [props, showText]
   );
-  return <Graphics draw={draw} />;
+  const handleMouseOver = (e: PIXI.InteractionEvent) => {
+    setShowText(true);
+  };
+  const handleMouseOut = (e: PIXI.InteractionEvent) => {
+    setShowText(false);
+  };
+  return (
+    <Graphics
+      draw={draw}
+      interactive={true}
+      mouseover={handleMouseOver}
+      mouseout={handleMouseOut}
+    />
+  );
 }
